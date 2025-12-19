923d0bc0190e28db9e74d3091baee76246123313
sarthak bansal <sarthakbansal@18thdigitech.net>
2025-12-19 14:57:23 +0530

    optimized

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ dotenv.config();
 const app = express();
 app.use(bodyParser.json());
 // app.use(cors({ origin: "http://localhost:3000" })); // or "*" for all origins
-app.use(cors({ origin: "https://juspaydemo1dep.vercel.app/" })); // or "*" for all origins
+app.use(cors({ origin: process.env.FRONTEND })); // or "*" for all origins
 let key;
 let mid;
 app.post("/create-session", async (req, res) => {
@@ -116,7 +116,7 @@ if (referenceId && REFERENCE_ID_TO_METADATA_KEY[referenceId]) {
       customer_phone: generateRandom()?.mobile || "9876543210",
       payment_page_client_id: process.env.JUSPAY_CLIENT_ID,
       action: "paymentPage",
-      return_url: "https://juspaydemo1dep.vercel.app/",
+      return_url: process.env.FRONTEND,
       // return_url: "http://localhost:3000",
       // // return_url: "http://localhost:4000/payment-response",
       description: "Complete your payment",
